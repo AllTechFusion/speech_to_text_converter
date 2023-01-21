e4ce4e3f2f3f95c50f60c2e63d908c7817c9b5cb
Vikrant <vikrantyt6380@gmail.com>
2023-01-22 00:03:40 +0530

    project speech to text

---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { SpeechRecognitionService } from './speech-recognition.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'speechToText';
+  text: string | undefined;
+  languageCode: { key: string; value: string; }[] = [];
+  selectedValue: any;
+
+  constructor(
+   public service:SpeechRecognitionService
+    ) {
+      this.service.init()
+     }
+   
+    ngOnInit(): void {
+      this.languageCode=[{
+        key:"marathi",
+        value:"mr"
+      },
+      {
+        key:"English",
+        value:"en-US"
+      },
+      {
+        key:"Hindi",
+        value:"hi"
+      },
+      {
+        key:"German (Standard)",
+        value:"de"
+      }
+    ]
+    }
+   
+    start(){
+      this.service.start()
+    }
+   
+    stop(){
+      this.service.stop()
+    }
+    clear(){
+      this.service.text="";
+    }
+    selectValue(){
+      this.service.recognition.lang=this.selectedValue;
+    }
+ 
+}
